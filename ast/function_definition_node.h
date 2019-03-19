f29cd95d8b3f038045bb1d567c4cb1cc9bf35295
sernobrega <sermnobrega@gmail.com>
2019-03-19 20:18:24 +0000

    Reorder

--- ast/function_definition_node.h
+++ ast/function_definition_node.h
@@ -3,7 +3,6 @@
 #define __M19_FUNCTIONDEFINITION_H__
 
 #include <cdk/ast/sequence_node.h>
-#include <cdk/ast/expression_node.h>
 #include <cdk/basic_type.h>
 #include <string>
 
@@ -14,11 +13,10 @@ namespace m19 {
    */
   class function_definition_node: public cdk::basic_node {
     int _scope;
-    std::string _id;
     basic_type *_type;
+    std::string _id;
     cdk::sequence_node * _arguments;
   
-
   public:
     inline function_definition_node(int lineno, int scope, basic_type *type, const std::string &id, cdk::sequence_node *arguments) :
         cdk::basic_node(lineno), _scope(scope), _type(type), _id(id), _arguments(arguments) {
@@ -29,12 +27,13 @@ namespace m19 {
       return _scope;
     }
 
-    inline std::string id() const {
-      return _id;
-    }
-
     inline basic_type * type() {
       return _type;
+    }
+
+
+    inline std::string id() const {
+      return _id;
     }
 
     inline cdk::sequence_node *arguments() {
