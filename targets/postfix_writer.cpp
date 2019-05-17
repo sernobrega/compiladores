0e64e55f4220344e76ef172ab55cd89747ccc8a6
sernobrega <sermnobrega@gmail.com>
2019-05-17 22:00:35 +0100

    Fix

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -310,19 +310,19 @@ void m19::postfix_writer::do_function_definition_node(m19::function_definition_n
   //FIXME: naive approach - what if functions are defined inside a block or in an argument
   bool isMain = (node->id() == "m19");
 
-  _function = new_symbol();
-  _functions_to_declare.erase(_function->name());
-  reset_new_symbol();
+  // _function = new_symbol();
+  // _functions_to_declare.erase(_function->name());
+  // reset_new_symbol();
 
-  _offset = 8; //FP IP
-  _symtab.push();
-  if(node->arguments()) {
-    for(size_t ix = 0; ix < node->arguments()->size(); ix++) {
-      cdk::basic_node * arg = node->arguments()->node(ix);
-      if(arg == nullptr) break;
-      arg->accept(this, 0);
-    }
-  }
+  // _offset = 8; //FP IP
+  // _symtab.push();
+  // if(node->arguments()) {
+  //   for(size_t ix = 0; ix < node->arguments()->size(); ix++) {
+  //     cdk::basic_node * arg = node->arguments()->node(ix);
+  //     if(arg == nullptr) break;
+  //     arg->accept(this, 0);
+  //   }
+  // }
 
   _pf.TEXT();
   _pf.ALIGN();
@@ -341,7 +341,7 @@ void m19::postfix_writer::do_function_definition_node(m19::function_definition_n
   //SOMETHING FOR ENTER
   _pf.ENTER(0);
 
-  _offset = -_function->type()->size(); //retval
+  // _offset = -_function->type()->size(); //retval
 
   //sections
   if(node->init()) node->init()->accept(this, lvl + 4);
@@ -364,7 +364,7 @@ void m19::postfix_writer::do_function_definition_node(m19::function_definition_n
   // _pf.RET();
   
   //main function (m19) is being defined, functions to be declared are extern
-  if(isMain) 
-    for(std::string s: _functions_to_declare)
-      _pf.EXTERN(s);
+  // if(isMain) 
+  //   for(std::string s: _functions_to_declare)
+  //     _pf.EXTERN(s);
 }
