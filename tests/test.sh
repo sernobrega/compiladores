216c3558d8cec971b77a824bb11d8ae571a03b8d
sernobrega <sermnobrega@gmail.com>
2019-05-18 21:55:01 +0100

    Final script

--- tests/test.sh
+++ tests/test.sh
@@ -1,12 +1,12 @@
 for f in *.m19; 
 do 
 	FILE="${f%%.*}"
+	echo "==== Creating ASM $FILE ====="
 	../m19 --target asm $f;
 	yasm -felf32 "$FILE.asm"
-	echo "Creating executable of $FILE"
 	ld -m elf_i386 -o "$FILE" "$FILE.o" -lrts
-	echo "Executing $FILE"
+	echo "==== Executing $FILE ====="
 	./$FILE > "$FILE.out"
-	diff "$FILE.out" excepted/"$FILE.out"
+	diff "$FILE.out" expected/"$FILE.out"
 done
 
