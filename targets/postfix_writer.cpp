92bae3d782a1a34ff8bbc581a85204a57bc17e40
sernobrega <sermnobrega@gmail.com>
2019-05-18 13:10:22 +0100

    Fix block typo

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -383,7 +383,7 @@ void m19::postfix_writer::do_section_init_node(m19::section_init_node * const no
 
 void m19::postfix_writer::do_block_node(m19::block_node * const node, int lvl) {
   _symtab.push(); // for block-local vars
-  if (node->declarations()) node->declarations()->accept(this, lvl + 2);
-  if (node->instructions()) node->instructions()->accept(this, lvl + 2);
+  if (node->declaration()) node->declaration()->accept(this, lvl + 2);
+  if (node->instruction()) node->instruction()->accept(this, lvl + 2);
   _symtab.pop();
 }
