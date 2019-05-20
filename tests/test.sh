79a0512d114181ebf7541a87ea817569257aaf65
sernobrega <sermnobrega@gmail.com>
2019-05-20 16:38:44 +0100

    Fix test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -1,12 +1,27 @@
+passed=0
+total=0
+
 for f in *.m19; 
 do 
 	FILE="${f%%.*}"
-	echo "==== Creating ASM $FILE ====="
 	../m19 --target asm $f;
 	yasm -felf32 "$FILE.asm"
 	ld -m elf_i386 -o "$FILE" "$FILE.o" -lrts
-	echo "==== Executing $FILE ====="
 	./$FILE > "$FILE.out"
-	diff -Z  "$FILE.out" expected/"$FILE.out"
+
+	DIFF=$(diff -Z  "$FILE.out" expected/"$FILE.out") 
+	if [ "$DIFF" != "" ] 
+	then
+		echo "Test $FILE didn't pass."
+	fi
+
+	if [ "$seconds" -eq 0 ]; then
+		echo "Test $FILE didn't pass."
+	else
+		passed=$((passed+1))
+	fi
+	
+	total=$((total+1))
 done
 
+echo "Passed: $passed/$topass"
