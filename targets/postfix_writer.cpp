c765272c716c3000524c78d7184aa7b2ec22ec2f
sernobrega <sermnobrega@gmail.com>
2019-05-21 19:05:16 +0100

    Try implement @

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -26,7 +26,6 @@ void m19::postfix_writer::do_variable_node(cdk::variable_node * const node, int 
   ASSERT_SAFE_EXPRESSIONS;
 
   if(node->name() == "@") {
-    _pf.ADDR(_function->name());
     _pf.LOCAL(_function->offset());
     return;
   }
