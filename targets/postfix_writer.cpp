26a04611b87ad774f6490d3d144609d6c1045169
sernobrega <sermnobrega@gmail.com>
2019-05-21 13:31:54 +0100

    Fix return value of functions

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -510,8 +510,14 @@ void m19::postfix_writer::do_function_definition_node(m19::function_definition_n
   _symtab.pop(); 
 
   _pf.LOCAL(_offset);
-  _pf.LDINT();
-  _pf.STFVAL32();
+  if(_function->type()->name() == basic_type::TYPE_INT || _function->type()->name() == basic_type::TYPE_POINTER || _function->type()->name() == basic_type::TYPE_STRING) {
+    _pf.LDINT();
+    _pf.STFVAL32();
+  } else if(_function->type()->name() == basic_type::TYPE_DOUBLE) {
+    _pf.LDDOUBLE();
+    _pf.STFVAL64();
+  }
+  
 
   _pf.LEAVE();
   _pf.RET();
