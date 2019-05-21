3838850687f81b92bd358afe6173a4a9c0e6260c
sernobrega <sermnobrega@gmail.com>
2019-05-21 15:08:15 +0100

    offset func call

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -490,18 +490,7 @@ void m19::postfix_writer::do_function_definition_node(m19::function_definition_n
 
   _inFunctionBody = true;
 
-  _offset = -_function->type()->size(); //retval
-  if(node->retval()) {
-      node->retval()->accept(this, lvl);
-  if(_function->type()->name() == basic_type::TYPE_INT || _function->type()->name() == basic_type::TYPE_POINTER || _function->type()->name() == basic_type::TYPE_STRING) {
-
-    _pf.STFVAL32();
-  } else if(_function->type()->name() == basic_type::TYPE_DOUBLE) {
-
-    _pf.STFVAL64();
-  }
-  }
-  
+  //_offset = -_function->type()->size(); //retval
 
   //sections
   os() << "        ;; before body " << std::endl;
@@ -517,9 +506,18 @@ void m19::postfix_writer::do_function_definition_node(m19::function_definition_n
   os() << "        ;; after body " << std::endl;
   _inFunctionBody = false;
 
-  
-
   _symtab.pop(); 
+
+  if(node->retval()) {
+      node->retval()->accept(this, lvl);
+  if(_function->type()->name() == basic_type::TYPE_INT || _function->type()->name() == basic_type::TYPE_POINTER || _function->type()->name() == basic_type::TYPE_STRING) {
+
+    _pf.STFVAL32();
+  } else if(_function->type()->name() == basic_type::TYPE_DOUBLE) {
+
+    _pf.STFVAL64();
+  }
+  }
 
   _pf.LEAVE();
   _pf.RET();
