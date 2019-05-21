80d4927422a710ffec2345ba029657064028a927
sernobrega <sermnobrega@gmail.com>
2019-05-21 14:17:55 +0100

    offset func call

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -581,13 +581,10 @@ void m19::postfix_writer::do_function_call_node(m19::function_call_node * const 
   
 
   basic_type *type = symbol->type();
-  _pf.LOCAL(symbol->offset());
   if (type->name() == basic_type::TYPE_INT || type->name() == basic_type::TYPE_POINTER || type->name() == basic_type::TYPE_STRING) {
-    _pf.LDINT();
     _pf.LDFVAL32();
   }
   else if (type->name() == basic_type::TYPE_DOUBLE) {
-    _pf.LDDOUBLE();
     _pf.LDFVAL64();
   }
   else {
