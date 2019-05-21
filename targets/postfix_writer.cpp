68a18ca44467760bb27b0e67aacf6efd71838a5e
sernobrega <sermnobrega@gmail.com>
2019-05-21 13:50:36 +0100

    Fix return call def node

--- targets/postfix_writer.cpp
+++ targets/postfix_writer.cpp
@@ -491,6 +491,9 @@ void m19::postfix_writer::do_function_definition_node(m19::function_definition_n
   _inFunctionBody = true;
 
   node->retval()->accept(this, lvl);
+  if(!_function->type()) {
+    _function->type(new basic_type(4, TYPE_INT));
+  }
   _offset = -_function->type()->size(); //retval
 
 
