cb9e2c9eae45ea767dc7951f11efa4574ada4b27
sernobrega <sermnobrega@gmail.com>
2019-05-22 01:06:22 +0100

    New test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -6,10 +6,20 @@ do
 	FILE="${f%%.*}"
 	../m19 --target asm $f;
 	yasm -felf32 "$FILE.asm"
+	if [[ "$?" -eq "0" ]]; then
+		printf "YASM: OK, " 
+	else 
+		printf "YASM: Failed, ";
+
 	ld -m elf_i386 -o "$FILE" "$FILE.o" -lrts
+	if [[ "$?" -eq "0" ]]; then
+		echo "LD: OK." 
+	else 
+		echo "LD: Failed.";
+
 	./$FILE > "$FILE.out"
 
-	DIFF=$(diff -Z  "$FILE.out" expected/"$FILE.out")
+	DIFF=$(diff -w -E -B  "$FILE.out" expected/"$FILE.out")
 
 	if [ "$DIFF" != "" ]
 	then
