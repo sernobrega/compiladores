6c12526fbb54c41c9abd678fb59e339ae359decf
sernobrega <sermnobrega@gmail.com>
2019-05-22 01:11:51 +0100

    Fix test.sh

--- tests/test.sh
+++ tests/test.sh
@@ -5,6 +5,12 @@ for f in *.m19;
 do 
 	FILE="${f%%.*}"
 	../m19 --target asm $f;
+	if [[ "$?" -eq "0" ]]; then
+	    printf "..... Compiler: OK, " 
+	  else 
+	    printf "..... Compiler: Failed, ";
+	fi
+	
 	yasm -felf32 "$FILE.asm"
 	if [[ "$?" -eq "0" ]]
 	then
