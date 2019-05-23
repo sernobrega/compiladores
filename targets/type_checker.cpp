630f9c07496b00c2c617f889b29c2cf752acd833
sernobrega <sermnobrega@gmail.com>
2019-05-23 01:14:16 +0100

    Pointer level assignment

--- targets/type_checker.cpp
+++ targets/type_checker.cpp
@@ -116,7 +116,25 @@ void m19::type_checker::do_assignment_node(cdk::assignment_node * const node, in
   } else if (node->lvalue()->type()->name() == basic_type::TYPE_POINTER) {
     //TODO: check pointer level
     if (node->rvalue()->type()->name() == basic_type::TYPE_POINTER) {
-      node->type(new basic_type(4, basic_type::TYPE_POINTER));
+      int lt = 0, rt = 0;
+      basic_type * ltype = node->lvalue()->type();
+      for(; ltype->name() == basic_type::TYPE_POINTER; lt++, ltype = ltype->_subtype);
+
+      basic_type * rtype = node->rvalue()->type();
+      for(; rtype->name() == basic_type::TYPE_POINTER; rt++, exprtype = rtype->_subtype);
+
+      bool compatible = (lt == rt - 1) && (rtype->name() != basic_type::TYPE_INT) || (lt == rt) && (rt == 0 || (rt != 0 && rtype->name() == ltype->name()));
+      if (!compatible) throw std::string("wrong assignment to pointer");
+
+      basic_type * pointertype = new basic_type(4, basic_type::TYPE_POINTER);
+      basic_type * subtypeholder;
+      pointertype->_subtype = subtypeholder;
+      for(; lt > 0; lt--, subtypeholder = pointertype_f->_subtype) {
+        basic_type * pointertype = new basic_type(4, basic_type::TYPE_POINTER);
+        subtypeholder->_subtype = new basic_type(4, basic_type::TYPE_POINTER);
+      }
+      
+      node->type(pointertype);
     } else if (node->rvalue()->type()->name() == basic_type::TYPE_INT) {
       //TODO: check that the integer is a literal and that it is zero
       node->type(new basic_type(4, basic_type::TYPE_POINTER));
