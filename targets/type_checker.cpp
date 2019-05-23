8f9012d40a4474fce13e86387cb2532d9c1e690f
sernobrega <sermnobrega@gmail.com>
2019-05-23 22:42:03 +0100

    nullprt to false

--- targets/type_checker.cpp
+++ targets/type_checker.cpp
@@ -383,8 +383,8 @@ void m19::type_checker::do_PIDExpression(cdk::binary_expression_node * const nod
     basic_type * rtype = node->right()->type();
     for(; rtype->name() == basic_type::TYPE_POINTER; rt++, rtype = rtype->_subtype);
 
-    bool notcompatible = (lt == rt - 1 && (rtype->name() != basic_type::TYPE_INT));
-      if (notcompatible) throw std::string("wrong assignment to pointer");
+    // bool notcompatible = (lt == rt - 1 && (rtype->name() != basic_type::TYPE_INT));
+    //   if (notcompatible) throw std::string("wrong assignment to pointer");
 
     basic_type * pointertype = new basic_type(4, basic_type::TYPE_POINTER);
     basic_type * subtypeholder = new basic_type(0, basic_type::TYPE_UNSPEC);
