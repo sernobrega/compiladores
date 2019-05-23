7918283a61b67ace6a0a7af66b14a231e9bee7ed
sernobrega <sermnobrega@gmail.com>
2019-05-23 22:21:25 +0100

    experiment with pointers

--- targets/type_checker.cpp
+++ targets/type_checker.cpp
@@ -124,12 +124,8 @@ void m19::type_checker::do_assignment_node(cdk::assignment_node * const node, in
       basic_type * rtype = node->rvalue()->type();
       for(; rtype->name() == basic_type::TYPE_POINTER; rt++, rtype = rtype->_subtype);
 
-      bool compatible = ((lt == rt - 1) && (rtype->name() != basic_type::TYPE_INT)) || ((lt == rt) && rtype->name() == ltype->name());
+      bool compatible = ((lt == rt - 1) && (rtype->name() == basic_type::TYPE_INT) && !_nullptr);
       if (!compatible) throw std::string("wrong assignment to pointer");
-
-      if(lt == rt-1 && !_nullptr) {
-        throw std::string("wrong assignment to pointer");
-      }
 
       basic_type * pointertype = new basic_type(4, basic_type::TYPE_POINTER);
       basic_type * subtypeholder = new basic_type(0, basic_type::TYPE_UNSPEC);
