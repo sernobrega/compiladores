fcb71e1e2f63e06d5b32c1856ffcf8deb247c39c
sernobrega <sermnobrega@gmail.com>
2019-05-23 09:14:54 +0100

    inttodouble

--- targets/postfix_writer.h
+++ targets/postfix_writer.h
@@ -60,10 +60,6 @@ namespace m19 {
     void do_int2double(basic_type * left, basic_type * right) {
       if(left->name() == basic_type::TYPE_DOUBLE && right->name() == basic_type::TYPE_INT)
         _pf.I2D();
-      else if (left->name() != right->name()) 
-        error(0, std::string("impossible conversion"));
-
-        
     }
 
   public:
